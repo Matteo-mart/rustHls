81a5a409810dd46bb0abe95e7a5b3d5a6071f788
MATTEO-mart <matteo.mrtinez@gmail.com>
2026-03-26 16:40:38 +0100

    Fonctionnel

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+use std::env;
 mod utils;
 mod execute;
 
-
 fn main() {
-    // println!("\nHello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        std::process::exit(1);
+    }
+
+    let chemin_video = &args[2];
+    // println!("Le chemin est {}", chemin_video);
 
     // let _affichage_dossier = utils::affichage_dossier::affichage_dossier();
     let _delete = utils::delete::delete();
     let _create = utils::create::create();
-    let _convert_to_hls = execute::convert_to_hls::convert_to_hls("video/groovy-all-videos-and-all-audios.mp4");
+    let _convert_to_hls = execute::convert_to_hls::convert_to_hls(chemin_video);
     // let _convert_to_hls = execute::convert_to_hls::convert_to_hls("video/test_comment_captions.mp4");
 
 }
